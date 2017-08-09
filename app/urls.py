7df2d4f3730ed09392297766bb9cea6d4ed178d6
anjali-pai <anjalipai8296@gmail.com>
2017-08-09 18:21:16 +0530

    Switching to cbvs

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 
-from .views import index, about_page_view
+from .views import Index, About_page_view
+
+#urlpatterns = [
+ #   url(r'^$', index),
+  #  url(r'^about', about_page_view),
+#]
+
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^about', about_page_view),
+    url(r'^$', Index.as_view()),
+     url(r'^about', About_page_view.as_view()),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-	return HttpResponse("My staticpages homepage.")
+#def index(request):
+	#return render(request,'app/index.html')
 
-def about_page_view(request):
-	return HttpResponse("About page.")
+#def about_page_view(request):
+ #   return render(request, 'app/about.html')
+
+
+from django.views.generic import TemplateView
+class Index(TemplateView):
+    template_name = "app/index.html"
+class About_page_view(TemplateView):
+    template_name = "app/about.html"
